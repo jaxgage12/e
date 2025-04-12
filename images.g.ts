d79eaa275ec0086fafb2536485d48767a1ab6b2c
jaxgage12 <jaxgage.12@gmail.com>
2025-04-11 19:09:48 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,60 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+.....22222....2...
+...223332322223...
+.....2326233323...
+.....2228623332...
+....22268623332...
+...2232683343332..
+..22333333455332..
+.233333333455131..
+2222222333455131..
+...22223334551111.
+....2223366551611.
+...23222688866811.
+..23222268888886..
+..2322222688886...
+.222222222266.....
+.......222211.....
+.....22682681.....
+....2268268861....
+...226822688811...
+...2245426888441..
+...2455546886441..
+....455542662441..
+.....4542222.11...
+.......22.23......
+.......23.23......
+......122.23......
+.....1525111......
+.....14541161.....
+....16111711611...
+....1677775511611.
+....16666477771661
+....11111111111111
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
